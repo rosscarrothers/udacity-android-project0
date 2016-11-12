6db6e7c54c614b5a0154eee1b90f0b1831d42deb
Ross Carrothers <ross.carrothers@gmail.com>
2016-11-11 19:42:45 -0500

    Created layout with buttons which display a toast when clicked

--- app/src/main/java/ca/pintsofwine/appportfolio/MainActivity.java
+++ app/src/main/java/ca/pintsofwine/appportfolio/MainActivity.java
@@ -2,12 +2,23 @@ package ca.pintsofwine.appportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BUTTON_CLICKED_MESSAGE = "This button will launch my %s project";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayMessage(View view) {
+        Button buttonClicked = (Button) view;
+        String toastMessage = String.format(BUTTON_CLICKED_MESSAGE, buttonClicked.getText());
+        Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
     }
 }
